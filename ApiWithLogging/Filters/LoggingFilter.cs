1cfe5f2f2a225dfc02b43a6f07840bd17f1a22be
DESKTOP-D1R0HI2\light <lighthead.dev@gmail.com>
2022-12-04 17:00:16 +0900

    Add LoggingFilter

---
+++ ApiWithLogging/Filters/LoggingFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiWithLogging.Filters;
+
+public class LoggingFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        var action = ((ControllerActionDescriptor)context.ActionDescriptor).ActionName;
+        var controller = ((ControllerBase)context.Controller).ControllerContext.ActionDescriptor.ControllerName;
+
+        Console.WriteLine($"Called before {action} {controller}");
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        var action = ((ControllerActionDescriptor)context.ActionDescriptor).ActionName;
+        var controller = ((ControllerBase)context.Controller).ControllerContext.ActionDescriptor.ControllerName;
+
+        Console.WriteLine($"Called after {action} {controller}");
+    }
+}
