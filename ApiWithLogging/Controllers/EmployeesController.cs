253f85997d5a74d3cda3609ad1d7f8ce20ce9354
DESKTOP-D1R0HI2\light <lighthead.dev@gmail.com>
2022-12-04 17:27:44 +0900

    Add EmployeesController

---
+++ ApiWithLogging/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using ApiWithLogging.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWithLogging.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private static readonly IReadOnlyList<Employee> Items = new List<Employee>()
+    {
+        new() {
+            Id = 1,
+            Name = "abc",
+        },
+        new() {
+            Id = 2,
+            Name = "bcd",
+        },
+    };
+
+    [HttpGet]
+    public IEnumerable<Employee> GetItems()
+    {
+        return Items.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public Employee GetItem(int id)
+    {
+        return Items.Single(x => x.Id == id);
+    }
+}
---
+++ ApiWithLogging/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiWithLogging.Models
+{
+    public class Employee
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+    }
+}
